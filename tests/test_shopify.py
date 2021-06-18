a51e5949d8e361c1305c122f94f35edfe48a58a1
Ken Van Haren <kenvanharen@gmail.com>
2021-06-17 17:27:05 -0700

    Fix tests

--- tests/test_shopify.py
+++ tests/test_shopify.py
@@ -1,8 +1,9 @@
 import os
+from dcp.storage.database.utils import get_tmp_sqlite_db_url
 
-from snapflow import Environment, graph, produce
 
 from loguru import logger
+from snapflow import Environment, DataspaceCfg, GraphCfg
 
 logger.enable("snapflow")
 
@@ -19,17 +20,20 @@ def test_shopify():
     from snapflow_shopify import module as shopify
 
     api_key = ensure_api_key()
-    env = Environment(metadata_storage="sqlite://")
-
-    g = graph()
+    storage = get_tmp_sqlite_db_url()
+    env = Environment(DataspaceCfg(metadata_storage="sqlite://", storages=[storage]))
 
     # Initial graph
-    orders = g.create_node(
-        "shopify.import_orders",
+    orders = GraphCfg(
+        key="import_orders",
+        function="shopify.import_orders",
         params={"admin_url": api_key},
     )
-    blocks = produce(orders, env=env, modules=[shopify])
-    records = blocks[0].as_records()
+    g = GraphCfg(nodes=[orders])
+    results = env.produce(
+        orders.key, g, target_storage=storage, execution_timelimit_seconds=1
+    )
+    records = results[0].stdout().as_records()
     assert len(records) > 0
 
 
